a113fa5db0309df8dbfd2d4bc77977906f556b40
Gabrielle Trotter <gtrotter@bulogics.com>
2015-12-01 11:11:29 -0500

    added module files for SevenSegmentDisplay module

---
+++ src/SevenSegDisplay.h
@@ -0,0 +1,76 @@
+/************************************************************************//**
+ *
+ * \file <FILENAME>
+ *
+ * \addtogroup <SevenSegDisplay> <SevenSegDisplay>
+ * \{
+ *
+ * \brief
+ *
+ * \note
+ *
+ * \author Gabrielle Trotter
+ * \date 12.1.15
+ *
+ ****************************************************************************/
+
+#ifndef SevenSegDisplay_H
+#define SevenSegDisplay_H
+
+/****************************************************************************
+ *                              INCLUDE FILES                               *
+ ****************************************************************************/
+
+/****************************************************************************
+ *                     EXPORTED TYPES and DEFINITIONS                       *
+ ****************************************************************************/
+#define SEGMENT_A 0x80
+#define SEGMENT_B 0x40
+#define SEGMENT_C 0x20
+#define SEGMENT_D 0x10
+#define SEGMENT_E 0x08
+#define SEGMENT_F 0x04
+#define SEGMENT_G 0x02
+#define SEGMENT_DP 0x01
+
+
+//Digit 2 Port / Pin Definitions
+#define D2_SEG_A_PIN GPIO_PIN7
+#define D1_SEG_B_PIN GPIO_PIN6
+#define D1_SEG_C_PIN GPIO_PIN5
+#define D1_SEG_D_PIN GPIO_PIN4
+#define D1_SEG_E_PIN GPIO_PIN3
+#define D1_SEG_F_PIN GPIO_PIN2
+#define D1_SEG_G_PIN GPIO_PIN1
+#define D1_SEG_DP_PIN GPIO_PIN0
+
+#define D1_SEG_A_PORT GPIO_PORTD
+#define D1_SEG_B_PORT GPIO_PORTD
+#define D1_SEG_C_PORT GPIO_PORTD
+#define D1_SEG_D_PORT GPIO_PORTD
+#define D1_SEG_E_PORT GPIO_PORTD
+#define D1_SEG_F_PORT GPIO_PORTD
+#define D1_SEG_G_PORT GPIO_PORTD
+#define D1_SEG_DP_PORT GPIO_PORTD
+
+typedef enum
+{
+    SSD_STATE_IDLE,
+    //more states should be added...
+} SSD_STATE;
+
+/****************************************************************************
+ *                              EXPORTED DATA                               *
+ ****************************************************************************/
+
+/****************************************************************************
+ *                     EXPORTED FUNCTION DECLARATIONS                       *
+ ****************************************************************************/
+void SSD_Init(void);
+void SSD_Tick(void);
+void SSD_SetDisplay(uint8_t id, uint8_t data);
+
+
+#endif // SevenSegDisplay_H
+
+/** \}*/
---
+++ test/test_SevenSegDisplay.c
@@ -0,0 +1,110 @@
+/************************************************************************//**
+ *
+ * \file test_SevenSegDisplay.h
+ *
+ * \brief Test scripts for module SevenSegDisplay to be run with Unity
+ *
+ * \note
+ *
+ * \author Gabrielle Trotter 
+ * \date 12.1.15
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ *                              INCLUDE FILES                               *
+ ****************************************************************************/
+#include "unity.h"
+#include "SevenSegDisplay.h"
+#include "MockGPIOHAL.h"
+
+/****************************************************************************
+ *                      PRIVATE TYPES and DEFINITIONS                       *
+ ****************************************************************************/
+
+/****************************************************************************
+ *                              PRIVATE DATA                                *
+ ****************************************************************************/
+
+/****************************************************************************
+ *                             EXTERNAL DATA                                *
+ ****************************************************************************/
+
+/****************************************************************************
+ *                     PRIVATE FUNCTION DECLARATIONS                        *
+ ****************************************************************************/
+
+/****************************************************************************
+ *                     EXPORTED FUNCTION DEFINITIONS                        *
+ ****************************************************************************/
+
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_InitFunctionSetsUpAllGPIOPinsCorrectly(void)
+{
+  	TEST_IGNORE_MESSAGE("Implement me!");
+}
+
+void test_SegmentACanBeTurnedOn(void)
+{
+    TEST_IGNORE_MESSEGE("Implement me!");
+}
+
+void test_SegmentBCanBeTurnedOn(void)
+{
+    TEST_IGNORE_MESSEGE("Implement me!");
+}
+
+void test_SegmentCCanBeTurnedOn(void)
+{
+    TEST_IGNORE_MESSEGE("Implement me!");
+}
+
+void test_SegmentDCanBeTurnedOn(void)
+{
+    TEST_IGNORE_MESSEGE("Implement me!");
+}
+
+void test_SegmentECanBeTurnedOn(void)
+{
+    TEST_IGNORE_MESSEGE("Implement me!");
+}
+
+void test_SegmentFCanBeTurnedOn(void)
+{
+    TEST_IGNORE_MESSEGE("Implement me!");
+}
+
+void test_SegmentGCanBeTurnedOn(void)
+{
+    TEST_IGNORE_MESSEGE("Implement me!");
+}
+
+void test_SegmentDPCanBeTurnedOn(void)
+{
+    TEST_IGNORE_MESSEGE("Implement me!");
+}
+
+void test_DisplayCanBeTurnedOff(void)
+{
+    TEST_IGNORE_MESSEGE("Implement me!");
+}
+
+/****************************************************************************
+ *                     PRIVATE FUNCTION DEFINITIONS                         *
+ ****************************************************************************/
+
+/************************************************************************//**
+ * \brief
+ * \param
+ * \return
+ ****************************************************************************/
+
+// End of file.
